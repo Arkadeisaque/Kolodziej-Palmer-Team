96b838af5f9fa7d8715e708f7650cadc93053967
Arkadeisaque <arkadiusz7@gmail.com>
2015-10-06 21:10:19 -0400

    Team assignment from week 03

---
+++ Documents/Fall 2015/CIT 260/Quest/src/buyui/cit260/quest/model/Game.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buyui.cit260.quest.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Arkadiusz
+ */
+public class Game implements Serializable{
+    
+    // class instance variables
+    private int totalTime; 
+
+    public Game(int totalTime) {
+        this.totalTime = totalTime;
+    }
+
+    public Game() {
+        
+    }
+    
+
+    public int getTotalTime() {
+        return totalTime;
+    }
+
+    public void setTotalTime(int totalTime) {
+        this.totalTime = totalTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" + "totalTime=" + totalTime + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 97 * hash + this.totalTime;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Game other = (Game) obj;
+        if (this.totalTime != other.totalTime) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}

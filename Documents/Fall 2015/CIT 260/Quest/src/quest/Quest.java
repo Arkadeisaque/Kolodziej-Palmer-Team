a99dd67f24da005f37507e9fc61f0f46e77ee2fe
Arkadeisaque <arkadiusz7@gmail.com>
2015-10-06 21:19:41 -0400

    Team assignment from week 03

---
+++ Documents/Fall 2015/CIT 260/Quest/src/quest/Quest.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quest;
+
+import buyui.cit260.quest.model.Game;
+
+/**
+ *
+ * @author Arkadiusz
+ */
+public class Quest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Game totalTime = new Game();
+        
+        totalTime.setTotalTime(12);
+        
+        String playerInfo = totalTime.toString();
+        System.out.println(playerInfo);
+    }
+    
+}

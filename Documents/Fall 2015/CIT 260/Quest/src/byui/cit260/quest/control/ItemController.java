a6264e1988da616f3c1a1a553ff8f038f84e7cef
Arkadeisaque <arkadiusz7@gmail.com>
2015-12-02 23:45:56 -0500

    Team assignment from week 03

--- Documents/Fall 2015/CIT 260/Quest/src/byui/cit260/quest/control/ItemController.java
+++ Documents/Fall 2015/CIT 260/Quest/src/byui/cit260/quest/control/ItemController.java
@@ -5,10 +5,29 @@
  */
 package byui.cit260.quest.control;
 
+import java.util.Arrays;
+
 /**
  *
  * @author Arkadiusz
  */
 public class ItemController {
-    
+        //Author of below Ryan
+
+    public static void main(String args[]) {
+
+        //String array
+        String[] strNames = new String[]{"Corn", "Orb", "Enchanted Apple", "Bucket", "Bucket With Water", "Gauntlets", "Torch", "Meat", "Knife", "Map"};
+
+        //sort String array using sort method
+        Arrays.sort(strNames);
+
+        System.out.println("String array sorted (case sensitive)");
+
+        //print sorted elements
+        for (int i = 0; i < strNames.length; i++) {
+            System.out.println(strNames[i]);
+        }
+
+    }
 }

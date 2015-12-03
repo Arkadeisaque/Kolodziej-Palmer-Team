0e3ac00f35b4cd0eedb6b4764b3ac7c5403b7703
Arkadeisaque <arkadiusz7@gmail.com>
2015-12-02 23:54:20 -0500

    Team assignment from week 03

---
+++ Documents/Fall 2015/CIT 260/Quest/test/byui/cit260/quest/control/TalkControllerTest.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.quest.control;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Arkadiusz
+ */
+public class TalkControllerTest {
+    
+    public TalkControllerTest() {
+    }
+
+    /**
+     * Test of talkToActor method, of class TalkController.
+     */
+//    @Test
+//    public void testTalkToActor() {
+//     System.out.println("talkToActor");
+//      location = null;
+//      TalkController instance = new TalkController();
+//      boolean expResult = false;
+ //      boolean result = instance.talkToActor(<error>);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//    
+}
